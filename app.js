ea40d1301f9861e4e3f5b54a1e6e51752b74f3f3
Dewa Bisma <idewagedebisma@gmail.com>
2020-11-18 22:01:50 +0800

    add json, function calculate, parsing data

--- app.js
+++ app.js
@@ -15,6 +15,24 @@ app.use(bodyParser.urlencoded({
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
 
+// Function
+const saveData = () => {
+    fetch('https://localhost:3000/perhitungan', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data),
+        })
+        .then(response => response.json())
+        .then(data => {
+            console.log('Success:', data);
+        })
+        .catch((error) => {
+            console.error('Error:', error);
+        });
+}
+
 // Set Up Route
 
 app.get('/', (req, res) => {
@@ -23,11 +41,43 @@ app.get('/', (req, res) => {
 
 app.post('/', (req, res) => {
     const data = req.body;
-    console.log(data);
+    switch (data.bangun) {
+        case "segitiga":
+            const newItem1 = {
+                'time': new Date().toLocaleString(),
+                'jenis': data.bangun,
+                'tinggi': data.tinggi,
+                'alas': data.alas,
+                'satuan': data.satuan,
+                'luas': ((data.alas*data.tinggi)/2).toFixed(2)
+            }
+            
+            break;
+        case "lingkaran":
+            const newItem2 = {
+                'time': new Date().toLocaleString(),
+                'jenis': data.bangun,
+                'jari-jari': data.jari_jari,
+                'satuan': data.satuan,
+                'luas': (3.14*Math.pow(data.jari_jari, 2)).toFixed(2)
+            }
+            
+            break;
+        case "persegi":
+            const newItem3 = {
+                'time': new Date().toLocaleString(),
+                'jenis': data.bangun,
+                'sisi': data.sisi,
+                'satuan': data.satuan,
+                'luas': Math.pow(data.sisi, 2).toFixed(2)
+            }
+        
+            break;
+    }
 })
 
 app.get('/test', (req, res) => {
-    fs.readFile(`${__dirname}/views/test.html`, function (err, data) {
+    fs.readFile(`${__dirname}/views/test.csv`, function (err, data) {
         res.writeHead(200, {
             'Content-Type': 'text/html'
         });
@@ -41,6 +91,6 @@ app.get('/dashboard', (req, res) => {
 })
 
 // Set Up Connection
-app.listen(3000, (req, res) => {
-    console.log("Connected at port 3000");
+app.listen(5050, (req, res) => {
+    console.log("Connected at port 5050");
 })
